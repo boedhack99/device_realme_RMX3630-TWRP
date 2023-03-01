fafe01586db72a1f332ffe19c0bb4b38b190a14c
boedhack99 <boedhack@gmail.com>
2023-03-01 20:07:17 +0700

    RMX3630: Build with vendor_ramdisk
    Change-Id: Ia4bd8e2cf103a344db4463426a547ba2da0dfc02

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 ENABLE_VIRTUAL_AB := true
 
