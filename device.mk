bf334d8ccb358f3adb6901c54e7fddea32901733
boedhack99 <boedhack@gmail.com>
2023-03-12 19:34:01 +0700

    RMX3630: Enable vA/B compression - Inherit vA/B compression - Build snapuserd
    Change-Id: I63760adfbfee0f1b475c7a7d6abe7b21497c8c7d

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 #
 
 # Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 ENABLE_VIRTUAL_AB := true
 
