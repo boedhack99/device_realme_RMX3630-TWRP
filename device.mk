568db13d2b47afa6d1c7c36c3765bd0e066e4679
boedhack99 <boedhack@gmail.com>
2023-03-14 19:25:47 +0700

    RMX3630: Back to include gsi_keys instead dev gsi
    Change-Id: Ia0c7b89fc935c5b3acadae49086d05933b085969

--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
